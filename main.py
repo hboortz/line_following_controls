1f639fcb38281830069d8cc378566df3eff73cb6
heather <heather.boortz@gmail.com>
2015-04-26 20:26:15 -0400

    adding motor control thing

--- main.py
+++ main.py
@@ -1,62 +1,88 @@
+#!/usr/bin/env python
+
+""" Example code of how to move a robot forward for 3 seconds. """
+
+# We always import roslib, and load the manifest to handle dependencies
+import roslib; roslib.load_manifest('mini_max_tutorials')
+import rospy
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+# recall: robots generally take base movement commands on a topic 
+#  called "cmd_vel" using a message type "geometry_msgs/Twist"
+from geometry_msgs.msg import Twist
 
-while True:
-    _, frame = cap.read()
-    cv2.imshow('frame',frame)
+x_speed = 0.1  # 0.1 m/s
 
-    # Take each frame
-    _, frame = cap.read()
+def mainloop(cap):
+    while True:
+        _, frame = cap.read()
+        cv2.imshow('frame',frame)
+        _, frame = cap.read()
 
-    # Convert BGR to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # Threshold to get black line
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        lower_bound = np.array([0,0,0])
+        upper_bound = np.array([255,255,100])
+        mask = cv2.inRange(hsv, lower_bound, upper_bound)
+        middle = mask.shape[0]/2
+        res = cv2.bitwise_and(frame,frame, mask=mask)
 
-    # define range of blue color in HSV
-    lower_bound = np.array([0,0,0])
-    upper_bound = np.array([255,255,100])
+        # Get contours and centroid
+        contours, heirarchy = cv2.findContours(mask, 1, 2)
+        cnt = contours[0]
+        momt = cv2.moments(cnt)
+        # print momt
+        try:
+            cx = int(momt['m10']/momt['m00'])
+            cy = int(momt['m01']/momt['m00'])
+            diff = middle - cx
+            print diff
+        except ZeroDivisionError:
+            pass
 
-    # Threshold the HSV image to get the black line
-    mask = cv2.inRange(hsv, lower_bound, upper_bound)
-    middle = mask.shape[0]/2
+        # display contours
+        # raw_input()
+        cv2.imshow('mask',mask)
 
-    # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(frame,frame, mask=mask)
-
-
-    contours, heirarchy = cv2.findContours(mask, 1, 2)
-    cnt = contours[0]
-    momt = cv2.moments(cnt)
-    print momt
-
-    try:
-        cx = int(momt['m10']/momt['m00'])
-        cy = int(momt['m01']/momt['m00'])
-        print cx, cy
-    except ZeroDivisionError:
-        pass
-    raw_input()
-    cv2.imshow('mask',mask)
-
-
-    # detector = cv2.SimpleBlobDetector()
-    
-    # # Detect blobs
-    # keypoints = detector.detect(im)
-    
-    # # Draw detected blobs as red circles.
-    # # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-    # im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    # cv2.imshow("Keypoints", im_with_keypoints)
-
-    # cv2.imshow('frame',frame)
-    # cv2.imshow('mask',mask)
-    # cv2.imshow('res',res)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def control_motors():
+    # first thing, init a node!
+    rospy.init_node('move')
 
-cap.release()
-cv2.destroyAllWindows()
+    # publish to cmd_vel
+    p = rospy.Publisher('cmd_vel', Twist)
+
+    # create a twist message, fill in the details
+    twist = Twist()
+    twist.linear.x = x_speed;                   # our forward speed
+    twist.linear.y = 0; twist.linear.z = 0;     # we can't use these!        
+    twist.angular.x = 0; twist.angular.y = 0;   #          or these!
+    twist.angular.z = 0;                        # no rotation
+
+    # announce move, and publish the message
+    rospy.loginfo("About to be moving forward!")
+    for i in range(30):
+        p.publish(twist)
+        rospy.sleep(0.1) # 30*0.1 = 3.0
+
+    # create a new message
+    twist = Twist()
+
+    # note: everything defaults to 0 in twist, if we don't fill it in, we stop!
+    rospy.loginfo("Stopping!")
+    p.publish(twist)
+
+def cleanup(cap):
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    # cap = cv2.VideoCapture(0)
+    # mainloop(cap)
+    # cleanup(cap)
+    control_motors()
